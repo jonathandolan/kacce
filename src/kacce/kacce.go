114e7aac404f14845a3c782866b5a4716373f67f
jonathandolan <jonathanddolan@gmail.com>
2015-02-18 12:44:25 -0700

    Switched packages for board representation.

--- src/kacce/kacce.go
+++ src/kacce/kacce.go
@@ -1,25 +1,10 @@
 package main
 
 import (
-	"bufio"
-	//"chess"
 	"fmt"
-	"os"
-	"strings"
+	//"github.com/malbrecht/chess"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	in, _ := reader.ReadString('\n')
-	in = strings.TrimSuffix(in, "\n")
-	if strings.EqualFold(in, "uci") {
-		fmt.Println("id name kacce")
-		fmt.Println("id author Jonathan Dolan")
-		fmt.Println("uciok")
-	}
-	in, _ = reader.ReadString('\n')
-	in = strings.TrimSuffix(in, "\n")
-	if strings.EqualFold(in, "isready") {
-		fmt.Println("readyok")
-	}
+	fmt.Println("asdf")
 }
